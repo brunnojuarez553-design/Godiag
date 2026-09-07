ac3c60a72ae7bf5eab5540e7f44f94bce94a17cb
brunnojuarez553-design <brunnojuarez553@gmail.com>
2026-09-07 16:52:58 -0300

    Load mobile composition and masonry styles

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,8 @@ import "./premium.css";
 import "./polish.css";
 import "./audit.css";
 import "./brand-marquee.css";
+import "./mobile-experience.css";
+import "./mobile-masonry.css";
 
 const logo = "https://res.cloudinary.com/dvvuwigmy/image/upload/v1788232533/IMG_1016_y4atye.jpg";
 const ogImage = "https://res.cloudinary.com/dvvuwigmy/image/upload/v1788231287/IMG_1017_rx0uzq.jpg";
