a1335b9d9502f6a1df2436a15e857645b7e97156
brunnojuarez553-design <brunnojuarez553@gmail.com>
2026-09-08 12:55:12 -0300

    Import orientation section background styles

--- app/layout.tsx
+++ app/layout.tsx
@@ -12,6 +12,7 @@ import "./mobile-masonry.css";
 import "./case-lightbox.css";
 import "./seo-pages.css";
 import "./mobile-fixes.css";
+import "./quiz-background.css";
 
 const logo = "https://res.cloudinary.com/dvvuwigmy/image/upload/v1788232533/IMG_1016_y4atye.jpg";
 const ogImage = "https://res.cloudinary.com/dvvuwigmy/image/upload/v1788231287/IMG_1017_rx0uzq.jpg";
